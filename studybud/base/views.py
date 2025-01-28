ebd1fe00f52834be406bc6fdb2d187ff7f4811fe
Swimz-88 <jhalbach@uncc.edu>
2025-01-28 15:53:09 -0500

    Django1 Commit | 1:04:25

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home Page')
 
-def room(request):
-    return HttpResponse("ROOM")
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python!'},
+    {'id': 2, 'name': 'Design With Me'},
+    {'id': 3, 'name': 'Frontend Devs'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
